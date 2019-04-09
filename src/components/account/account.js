4bd477ad00c7c72e9d405d7ac27486eca86f168a
Tyrel Huseby <thuseby.business@gmail.com>
2019-04-09 14:33:52 -0600

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -10,6 +10,19 @@ class Account extends Component {
 
     componentDidMount() {
 
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Shop',
+                path: '/shop'
+            },
+            {
+                _id: 1,
+                title: 'Logout',
+                path: '/'
+            }
+        ]
+
         const navbarLinks = [
             {
                 _id: 0,
@@ -25,7 +38,7 @@ class Account extends Component {
             }
         ]
 
-        this.props.setHeaderLinks([]);
+        this.props.setHeaderLinks(headerLinks);
         this.props.setNavbarLinks(navbarLinks);
     }
 
@@ -57,4 +70,4 @@ function mapStateToProps(state) {
 
 Account = connect(mapStateToProps, actions)(Account);
 
-export default Account; 
+export default Account;
