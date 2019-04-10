0ec9899eeac924468d5886960b94ab440a6f1229
Tyrel Huseby <thuseby.business@gmail.com>
2019-04-10 14:05:41 -0600

    built base for shop component. built shop route etc

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,7 +7,6 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
-
 import './style/main.scss';
 
 import history from './history';
@@ -16,9 +15,7 @@ import Layout from './components/layout';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import Account from './components/account/account';
-
-
-
+import Shop from './components/shop/shop'
 
 function main() {
   ReactDOM.render(
@@ -27,10 +24,13 @@ function main() {
         <Layout>
           <Switch>
             <Route path='/' exact component={Signin}/>
+
             <Route path='/signin' exact component={Signin}/>
             <Route path='/signup' exact component={Signup}/>
 
             <Route path='/account' exact component={Account}/>
+
+            <Route path='/shop' exact component={Shop}/>
           </Switch>
         </Layout>
         </Router>
