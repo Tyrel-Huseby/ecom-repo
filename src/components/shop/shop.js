6cbd78eb79bcb57bc6f4077b70d254221f02c95e
Tyrel Huseby <thuseby.business@gmail.com>
2019-04-10 14:53:45 -0600

    built green price tag component and quantity component and added remaining jsx to product component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -3,6 +3,72 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
+import ShopProduct from './shopProduct';
+
+class Shop extends Component {
+
+    componentDidMount() {
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Login',
+                path: '/signin'
+            }
+        ]
+        this.props.setHeaderLinks(headerLinks);
+        this.props.fetchShopCategories();
+
+        // filter products with links
+        this.props.fetchShopProducts();
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if(this.props != nextProps) {
+            this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
+        }
+        return true
+    }
+
+    onSubmit = (fields) => {
+        this.props.filterProductsWithQuery(fields)
+    }
+
+    render() {
+
+        return (
+            <div className='shop'>
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
+                <div className='shop__products'>
+                    {
+                        this.props.filteredProducts.map(product => {
+                            return (
+                                <ShopProduct {...product} key={product._id} />
+                            )
+                        })
+                    }
+                </div>
+                {/* shop cart button */}
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { categories, filteredProducts } = state.shop;
+    return {
+        categories,
+        filteredProducts
+    } 
+}
+
+Shop = connect(mapStateToProps, actions)(Shop);
+
+export default Shop;
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import ShopSearchBar from './shopSearchBar';
 
 class Shop extends Component {
 
