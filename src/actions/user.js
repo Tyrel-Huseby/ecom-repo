dd6baabdf17c1b7bdd030fc2ef25efbac0a0809e
Tyrel Huseby <thuseby.business@gmail.com>
2019-04-12 10:19:06 -0600

    setup cartproduct model and action creator to fetch and set user cart products

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,13 +1,45 @@
 import { 
-	SET_USER_PURCHASES,
-	SET_PURCHASE_DETAIL
+    SET_USER_PURCHASES,
+    SET_PURCHASE_DETAIL,
+    SET_CART_PRODUCTS
 } from './types';
 
-export function setPurchaseDetail (_id) {
-	return({
-		type: SET_PURCHASE_DETAIL,
-		payload: _id
-	})
+export function setPurchaseDetail(_id) {
+    return ({
+        type: SET_PURCHASE_DETAIL,
+        payload: _id
+    })
+}
+
+
+export function fetchCartProducts() {
+    return ({
+        type: SET_CART_PRODUCTS,
+        payload: [
+            {
+                _id: 0,
+                product: {
+                    _id: 0,
+                    title: 'JavaScript in the Browser',
+                    description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                    price: 1.99,
+                    belongsTo: [0, 1]
+                },
+                quantity: 2
+            },
+            {
+                _id: 1,
+                product: {
+                    _id: 1,
+                    title: 'Graph Database',
+                    description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                    price: 1.99,
+                    belongsTo: [0, 6]
+                },
+                quantity: 1
+            },
+        ]
+    })
 }
 
 export function fetchUserPurchases() {
@@ -18,7 +50,7 @@ export function fetchUserPurchases() {
                 _id: 0,
                 total: 19.40,
                 orderNumber: 'A0048248343',
-                orderDate: new Date().toDateString,
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jordan Hudgens',
@@ -29,7 +61,7 @@ export function fetchUserPurchases() {
                 _id: 1,
                 total: 1.30,
                 orderNumber: '24343sdfkjh72',
-                orderDate: new Date().toDateString,
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Maxwell Nelson',
@@ -40,7 +72,7 @@ export function fetchUserPurchases() {
                 _id: 2,
                 total: 1.30,
                 orderNumber: 'B0048248343',
-                orderDate: new Date().toDateString,
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jordan Hudgens',
@@ -50,8 +82,8 @@ export function fetchUserPurchases() {
             {
                 _id: 3,
                 total: 16.40,
-                orderNumber: 'AasdredDFasdlfkjwe',
-                orderDate: new Date().toDateString,
+                orderNumber: 'ADFasdlfkjwe',
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Max Asher Nelson',
@@ -61,8 +93,8 @@ export function fetchUserPurchases() {
             {
                 _id: 4,
                 total: 19.40,
-                orderNumber: 'sdersd3',
-                orderDate: new Date().toDateString,
+                orderNumber: 'A0048248343',
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jordan Hudgens',
@@ -72,8 +104,8 @@ export function fetchUserPurchases() {
             {
                 _id: 5,
                 total: 19.40,
-                orderNumber: 'A0048sertsd248343',
-                orderDate: new Date().toDateString,
+                orderNumber: 'A0048248343',
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jordan Hudgens',
@@ -83,8 +115,8 @@ export function fetchUserPurchases() {
             {
                 _id: 6,
                 total: 19.40,
-                orderNumber: 'A004e442348248343',
-                orderDate: new Date().toDateString,
+                orderNumber: 'A0048248343',
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jordan Hudgens',
@@ -95,7 +127,7 @@ export function fetchUserPurchases() {
                 _id: 7,
                 total: 3.20,
                 orderNumber: '5UFISHDFJASUSHI298',
-                orderDate: new Date().toDateString,
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Andrew Wold',
@@ -105,4 +137,4 @@ export function fetchUserPurchases() {
           
         ]
     })
-} 
+}
